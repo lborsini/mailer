7d48f00b3f3137991b75f178ac3067687c4aef11
lborsini <122235229+lborsini@users.noreply.github.com>
2023-02-06 13:23:48 +0000

    Implementacion de busqueda de contenido y sendgrid

--- appMailer/__init__.py
+++ appMailer/__init__.py
@@ -6,6 +6,7 @@ def createApp():
     app = Flask(__name__)
 
     app.config.from_mapping(
+        FROM_EMAIL = os.environ.get('FROM_EMAIL'),
         SENDGRID_KEY = os.environ.get('SENDGRID_API_KEY'),
         SECRET_KEY = os.environ.get('SECRET_KEY'),
         DATABASE_HOST=os.environ.get('FLASK_DATABASE_HOST'),
@@ -17,6 +18,10 @@ def createApp():
     from . import db
 
     db.init_app(app)
+    
+    from . import mail
+
+    app.register_blueprint(mail.bp)
     
     return app
 
